1267e30caa367c6228d8316b9cee7caf8c011c3f
itcast <itheima@itcast.cn>
2022-09-10 15:18:43 +0800

    完成了商品功能开发

--- src/main.js
+++ src/main.js
@@ -13,14 +13,37 @@ import TreeTable from 'vue-table-with-tree-grid'
 import axios from 'axios'
 import request from '@/utils/request'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 Vue.prototype.$axios = request
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+Vue.component('tree-table', TreeTable)
+// Vue.use(TreeTable)
+// 将文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
-
-Vue.component('tree-table', TreeTable)
-// Vue.use(TreeTable)
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import Rights from '@/views/power/Rights.vue'
 import Roles from '@/views/power/Roles.vue'
 import Cate from '@/views/goods/Cate.vue'
 import Params from '@/views/goods/Params.vue'
+import GoodsList from '@/views/goods/List.vue'
+import Add from '@/views/goods/Add.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -24,7 +26,9 @@ const router = new VueRouter({
         { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
         { path: '/categories', component: Cate },
-        { path: '/params', component: Params }
+        { path: '/params', component: Params },
+        { path: '/goods', component: GoodsList },
+        { path: '/goods/add', component: Add }
       ]
     }
   ]
